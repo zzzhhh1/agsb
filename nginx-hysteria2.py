fa3b6895303c1bc4a8e92900ed4a41e32e036720
zzzhhh1 <58203962+zzzhhh1@users.noreply.github.com>
2025-06-25 09:56:55 +0800

    Update nginx-hysteria2.py

--- nginx-hysteria2.py
+++ nginx-hysteria2.py
@@ -3649,9 +3649,8 @@ show_menu() {{
     echo "6️⃣  删除服务"
     echo "0️⃣  退出"
     echo ""
-    echo "👨‍💻 GitHub: https://github.com/zhumengkang/"
-    echo "📺 YouTube: https://www.youtube.com/@康康的V2Ray与Clash"
-    echo "💬 Telegram: https://t.me/+WibQp7Mww1k5MmZl"
+    echo "📺 YouTube: https://www.youtube.com/@kejigongxiang"
+    echo "💬 Telegram: https://t.me/kjgxZY"
     echo ""
 }}
 
